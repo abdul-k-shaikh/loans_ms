1a391e036e02d87c907cb1421d4fb480b427331d
theabdul97@gmail.com <theabdul97@gmail.com>
2024-08-18 01:26:46 +0530

    @EnableJpaAuditing_CreateAPI_Repo_Service_Impl_AuditorAwareImpl

--- src/main/java/com/eazybytes/loans/controller/LoansController.java
+++ src/main/java/com/eazybytes/loans/controller/LoansController.java
@@ -1,9 +1,16 @@
 package com.eazybytes.loans.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eazybytes.loans.constants.LoansConstants;
+import com.eazybytes.loans.dto.ResponseDto;
 import com.eazybytes.loans.service.ILoansService;
 
 import lombok.AllArgsConstructor;
@@ -19,4 +26,10 @@ public class LoansController {
 
 	private ILoansService iLoanService;
 
+	@PostMapping("/create")
+	public ResponseEntity<ResponseDto>createLoan(@RequestParam String mobileNumber){
+		iLoanService.createLoan(mobileNumber);
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(new ResponseDto(LoansConstants.STATUS_201, LoansConstants.MESSAGE_201));
+	}
 }
