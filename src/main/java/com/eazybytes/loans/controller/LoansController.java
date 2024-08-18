97a634c7e95425337294eb5c95a182b0bc04458f
theabdul97@gmail.com <theabdul97@gmail.com>
2024-08-18 12:36:44 +0530

    CreatAPI_OpenApiDocs

--- src/main/java/com/eazybytes/loans/controller/LoansController.java
+++ src/main/java/com/eazybytes/loans/controller/LoansController.java
@@ -1,7 +1,6 @@
 package com.eazybytes.loans.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,9 +9,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eazybytes.loans.constants.LoansConstants;
+import com.eazybytes.loans.dto.ErrorResponseDto;
 import com.eazybytes.loans.dto.ResponseDto;
 import com.eazybytes.loans.service.ILoansService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 
 /**
@@ -25,7 +30,25 @@ import lombok.AllArgsConstructor;
 public class LoansController {
 
 	private ILoansService iLoanService;
-
+	
+	@Operation(
+            summary = "Create Loan REST API",
+            description = "REST API to create new loan inside EazyBank"
+    )
+	@ApiResponses({
+        @ApiResponse(
+                responseCode = "201",
+                description = "HTTP Status CREATED"
+        ),
+        @ApiResponse(
+                responseCode = "500",
+                description = "HTTP Status Internal Server Error",
+                content = @Content(
+                        schema = @Schema(implementation = ErrorResponseDto.class)
+                )
+        )
+}
+)
 	@PostMapping("/create")
 	public ResponseEntity<ResponseDto>createLoan(@RequestParam String mobileNumber){
 		iLoanService.createLoan(mobileNumber);
