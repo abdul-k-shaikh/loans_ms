9fc6a34376a778ed4070af59909d16a7e6d56bff
theabdul97@gmail.com <theabdul97@gmail.com>
2024-08-19 11:47:03 +0530

    FetchAPI_withCustomeException_AndMapper

--- src/main/java/com/eazybytes/loans/service/impl/LoansServiceImpl.java
+++ src/main/java/com/eazybytes/loans/service/impl/LoansServiceImpl.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 import com.eazybytes.loans.constants.LoansConstants;
 import com.eazybytes.loans.dto.LoansDto;
 import com.eazybytes.loans.entity.Loans;
+import com.eazybytes.loans.exception.LoanAlreadyExistsException;
+import com.eazybytes.loans.exception.ResourceNotFoundException;
+import com.eazybytes.loans.mapper.LoansMapper;
 import com.eazybytes.loans.repository.LoansRepository;
 import com.eazybytes.loans.service.ILoansService;
 
@@ -19,40 +22,42 @@ public class LoansServiceImpl implements ILoansService {
 
 	private LoansRepository loansRepository;
 
-	
 	/**
-	 *@param mobileNumber - Mobile Number of the Customer
+	 * @param mobileNumber - Mobile Number of the Customer
 	 */
 	@Override
 	public void createLoan(String mobileNumber) {
 		Optional<Loans> optionalLoans = loansRepository.findByMobileNumber(mobileNumber);
-		if(optionalLoans.isPresent()) {
-			 System.out.println("Loan already registered with given mobileNumber ");
-			 return;
+		if (optionalLoans.isPresent()) {
+			throw new LoanAlreadyExistsException("Loan already registered with given mobileNumber " + mobileNumber);
+
 		}
 		loansRepository.save(createNewLoan(mobileNumber));
-//		loansRepository.save(createNewLoan(mobileNumber));
 	}
+
 	/**
-     * @param mobileNumber - Mobile Number of the Customer
-     * @return the new loan details
-     */
+	 * @param mobileNumber - Mobile Number of the Customer
+	 * @return the new loan details
+	 */
 	private Loans createNewLoan(String mobileNumber) {
 		Loans newLoan = new Loans();
-        long randomLoanNumber = 100000000000L + new Random().nextInt(900000000);
-        newLoan.setLoanNumber(Long.toString(randomLoanNumber));
-        newLoan.setMobileNumber(mobileNumber);
-        newLoan.setLoanType(LoansConstants.HOME_LOAN);
-        newLoan.setTotalLoan(LoansConstants.NEW_LOAN_LIMIT);
-        newLoan.setAmountPaid(0);
-        newLoan.setOutstandingAmount(LoansConstants.NEW_LOAN_LIMIT);
-        return newLoan;
+		long randomLoanNumber = 100000000000L + new Random().nextInt(900000000);
+		newLoan.setLoanNumber(Long.toString(randomLoanNumber));
+		newLoan.setMobileNumber(mobileNumber);
+		newLoan.setLoanType(LoansConstants.HOME_LOAN);
+		newLoan.setTotalLoan(LoansConstants.NEW_LOAN_LIMIT);
+		newLoan.setAmountPaid(0);
+		newLoan.setOutstandingAmount(LoansConstants.NEW_LOAN_LIMIT);
+		return newLoan;
 	}
 
 	@Override
 	public LoansDto fetchLoan(String mobileNumber) {
-		// TODO Auto-generated method stub
-		return null;
+		Loans loans =  loansRepository.findByMobileNumber(mobileNumber).orElseThrow(
+				()->new ResourceNotFoundException("Loan", "mobileNumber", mobileNumber)
+				);
+		return LoansMapper.mapToLoansDto(loans, new LoansDto());
+		
 	}
 
 	@Override
